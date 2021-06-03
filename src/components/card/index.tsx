2d397069ea3bc5b331d62e5a430ef6c41b3c79ea
allexis096 <allexissoares@gmail.com>
2021-06-03 17:02:03 -0300

    :sparkles: feat: add card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import * as S from './styles';
+import theme from '../../styles/theme';
+import OvalSvg from '../../../assets/images/Oval.png';
+
+type CardProps = {
+  title: string;
+  subtitle: string;
+  pageCount: string;
+  imgUrl: string;
+};
+
+function Card({ title, subtitle, pageCount, imgUrl }: CardProps) {
+  return (
+    <S.CardView>
+      <Image source={OvalSvg} style={{ position: 'absolute', bottom: 0 }} />
+      <S.TextCardView>
+        <S.TextCardTitle>{title}</S.TextCardTitle>
+        <S.TextCardSubtitle>{subtitle}</S.TextCardSubtitle>
+
+        <S.CardLengthView>
+          <MaterialIcons
+            name="insert-chart-outlined"
+            size={20}
+            color={theme.colors.white175}
+          />
+          <S.TextCardBookLength>{pageCount} Read Now</S.TextCardBookLength>
+        </S.CardLengthView>
+      </S.TextCardView>
+
+      <Image
+        style={{
+          width: 72,
+          height: 111,
+          borderRadius: 5,
+        }}
+        source={{
+          uri: imgUrl,
+        }}
+      />
+    </S.CardView>
+  );
+}
+
+export { Card };
---
+++ src/components/card/styles.ts
@@ -0,0 +1,49 @@
+import styled, { css } from 'styled-components/native';
+
+export const CardView = styled.View`
+  ${({ theme }) => css`
+    width: 272px;
+    height: 139px;
+    background-color: #00173d;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 15px 20px 13px;
+    border-radius: 5px;
+    position: relative;
+  `};
+`;
+
+export const TextCardView = styled.View`
+  ${({ theme }) => css``};
+`;
+
+export const TextCardTitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.white75};
+    font-family: ${theme.font.playFairBold};
+    font-size: 27px;
+  `};
+`;
+
+export const TextCardSubtitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.white125};
+    font-size: 14px;
+    font-style: italic;
+    margin-top: 5px;
+  `};
+`;
+
+export const CardLengthView = styled.View`
+  ${({ theme }) => css`
+    margin-top: 30px;
+    flex-direction: row;
+    align-items: center;
+  `};
+`;
+
+export const TextCardBookLength = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.white125};
+  `};
+`;
