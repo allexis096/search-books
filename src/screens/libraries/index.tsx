1c7ff26a0ced48d6b43d780de1128801a056dcf3
allexis096 <allexissoares@gmail.com>
2021-06-03 17:01:15 -0300

    :sparkles: feat: add screens

---
+++ src/screens/libraries/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+import { Text } from 'react-native';
+
+function Libraries() {
+  return (
+    <S.Container>
+      <Text>Libraries</Text>
+    </S.Container>
+  );
+}
+
+export { Libraries };
---
+++ src/screens/profile/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+import { Text } from 'react-native';
+
+function Profile() {
+  return (
+    <S.Container>
+      <Text>Profile</Text>
+    </S.Container>
+  );
+}
+
+export { Profile };
