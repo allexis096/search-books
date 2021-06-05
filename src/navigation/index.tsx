2e63fbec64f94aa5f370466fc3c828d5f7d820da
allexis096 <allexissoares@gmail.com>
2021-06-05 12:34:29 -0300

    :zap: chore: update dependencies

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,14 +1,24 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  NavigationProp,
+  ParamListBase,
+  Route,
+} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 import { Home } from '../screens/home';
+import { Detail } from '../screens/detail';
 import { Libraries } from '../screens/libraries';
 import { Profile } from '../screens/profile';
+
 import theme from '../styles/theme';
 
 const icons = {
@@ -26,7 +36,24 @@ const icons = {
   },
 };
 
+type StackNavigationProps = {
+  route: Route<string>;
+  navigation: NavigationProp<ParamListBase>;
+};
+
 type RouteProps = 'Home' | 'Libraries' | 'Profile';
+
+function StackNavigation() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ header: () => null }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Detail" component={Detail} />
+    </Stack.Navigator>
+  );
+}
 
 function Navigation() {
   return (
@@ -46,7 +73,17 @@ function Navigation() {
           labelPosition: 'below-icon',
         }}
       >
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen
+          name="Home"
+          component={StackNavigation}
+          options={(navigation) => {
+            const focusedRoute = getFocusedRouteNameFromRoute(navigation.route);
+
+            return {
+              tabBarVisible: focusedRoute === 'Detail' ? false : true,
+            };
+          }}
+        />
         <Tab.Screen name="Libraries" component={Libraries} />
         <Tab.Screen name="Profile" component={Profile} />
       </Tab.Navigator>
