7c93d419e473d886bd924ae6385d6d2d40b3e140
allexis096 <allexissoares@gmail.com>
2021-06-05 12:36:03 -0300

    :zap: chore: update home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-native-snap-carousel';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
 import { Image, ScrollView, View } from 'react-native';
 
 import theme from '../../styles/theme';
@@ -11,12 +12,16 @@ import { Card, CardProps } from '../../components/card';
 import { CurrentlyReading } from '../../components/reading';
 
 import * as S from './styles';
+import { Search } from '../search';
 
 type CarouselProps = {
   item: CardProps;
 };
 
 function Home() {
+  const [hasText, setHasText] = useState(false);
+  const navigator = useNavigation();
+
   const renderItem = ({ item }: CarouselProps) => {
     return (
       <Card
@@ -28,67 +33,79 @@ function Home() {
     );
   };
 
+  function handleChangeText(text: string) {
+    if (text?.length > 0) {
+      return setHasText(true);
+    }
+
+    setHasText(false);
+  }
+
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <S.Container>
-        <S.InputView>
-          <Feather
-            name="search"
-            color={theme.colors.white100}
-            size={16}
-            style={{ position: 'absolute', top: 65, left: 15, zIndex: 10 }}
-          />
-          <S.Input
-            placeholder="Search book"
-            placeholderTextColor={theme.colors.black100}
-          />
-        </S.InputView>
+    <S.Container>
+      <S.InputView>
+        <Feather
+          name="search"
+          color={theme.colors.white100}
+          size={16}
+          style={{ position: 'absolute', top: 65, left: 15, zIndex: 10 }}
+        />
+        <S.Input
+          placeholder="Search book"
+          placeholderTextColor={theme.colors.black100}
+          onChangeText={handleChangeText}
+        />
+      </S.InputView>
+      {!hasText ? (
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <S.NameView>
+            <S.Text>
+              Hi, <S.DecoratedText>Mehmed Al Fatih</S.DecoratedText> 👋
+            </S.Text>
+          </S.NameView>
 
-        <S.NameView>
-          <S.Text>
-            Hi, <S.DecoratedText>Mehmed Al Fatih</S.DecoratedText> 👋
-          </S.Text>
-        </S.NameView>
+          <S.SectionView>
+            <S.TitleSectionView>
+              <S.TitleTextSection>Discover new book</S.TitleTextSection>
+              <S.ButtonSection>More</S.ButtonSection>
+            </S.TitleSectionView>
 
-        <S.SectionView>
-          <S.TitleSectionView>
-            <S.TitleTextSection>Discover new book</S.TitleTextSection>
-            <S.ButtonSection>More</S.ButtonSection>
-          </S.TitleSectionView>
+            <View style={{ marginLeft: -90 }}>
+              <Carousel
+                layout="default"
+                data={mock}
+                renderItem={renderItem}
+                sliderWidth={450}
+                itemWidth={270}
+              />
+            </View>
+          </S.SectionView>
 
-          <View style={{ marginLeft: -90 }}>
-            <Carousel
-              layout="default"
-              data={mock}
-              renderItem={renderItem}
-              sliderWidth={450}
-              itemWidth={270}
+          <S.SectionView>
+            <S.TitleSectionView style={{ marginBottom: 30 }}>
+              <S.TitleTextSection>Currently Reading</S.TitleTextSection>
+              <S.ButtonSection>All</S.ButtonSection>
+            </S.TitleSectionView>
+
+            <CurrentlyReading />
+          </S.SectionView>
+
+          <S.SectionView>
+            <S.TitleSectionView style={{ marginBottom: 30 }}>
+              <S.TitleTextSection>Reviews of The Days</S.TitleTextSection>
+              <S.ButtonSection>All Video</S.ButtonSection>
+            </S.TitleSectionView>
+
+            <Image
+              source={DontMakeThinkImage}
+              style={{ width: 335, height: 181 }}
             />
-          </View>
-        </S.SectionView>
-
-        <S.SectionView>
-          <S.TitleSectionView style={{ marginBottom: 30 }}>
-            <S.TitleTextSection>Currently Reading</S.TitleTextSection>
-            <S.ButtonSection>All</S.ButtonSection>
-          </S.TitleSectionView>
-
-          <CurrentlyReading />
-        </S.SectionView>
-
-        <S.SectionView>
-          <S.TitleSectionView style={{ marginBottom: 30 }}>
-            <S.TitleTextSection>Reviews of The Days</S.TitleTextSection>
-            <S.ButtonSection>All Video</S.ButtonSection>
-          </S.TitleSectionView>
-
-          <Image
-            source={DontMakeThinkImage}
-            style={{ width: 335, height: 181 }}
-          />
-        </S.SectionView>
-      </S.Container>
-    </ScrollView>
+          </S.SectionView>
+        </ScrollView>
+      ) : (
+        <Search />
+      )}
+    </S.Container>
   );
 }
 
