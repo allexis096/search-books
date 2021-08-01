76761a25e5414e6b93086d85dc68dfb706f2dbb9
allexis <allexissoares@gmail.com>
2021-08-01 18:48:43 -0300

    :wrench: chore: update some config files

--- src/screens/profile/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components/native';
-
-export const Container = styled.View`
-  ${({ theme }) => css`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-  `};
-`;
