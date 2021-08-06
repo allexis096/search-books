094eee2030a65c08392333862fba7647db5bb15a
allexis <allexissoares@gmail.com>
2021-08-05 23:18:13 -0300

    :white_check_mark: test: make test when has no input value and navigate to another screen

--- src/screens/welcome/test.tsx
+++ src/screens/welcome/test.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react-native';
+import { Alert } from 'react-native';
 
 import { WelcomeScreen } from '.';
 import { renderWithTheme } from '../../utils/renderWithTheme';
 
+const mockNavigator = jest.fn();
+
 jest.mock('@react-navigation/native', () => ({
-  useNavigation: () => jest.fn(),
+  useNavigation: () => ({
+    navigate: mockNavigator,
+  }),
 }));
+
+jest.spyOn(Alert, 'alert');
 
 describe('<Welcome />', () => {
   it('should render the screen', () => {
@@ -20,5 +28,29 @@ describe('<Welcome />', () => {
     expect(title.props.children).toMatch('What is your name?');
     expect(input.props.placeholder).toBe('Your name');
     expect(button.props.children).toBe('Send');
+  });
+
+  it('should render error when input has no value', async () => {
+    const { getByText } = renderWithTheme(<WelcomeScreen />);
+
+    const button = getByText('Send');
+
+    fireEvent.press(button);
+
+    expect(Alert.alert).toHaveBeenCalledWith('You need to put a name, dude.');
+  });
+
+  it('should navigate to another screen', async () => {
+    const { getByText, getByPlaceholderText } = renderWithTheme(
+      <WelcomeScreen />
+    );
+
+    const input = getByPlaceholderText('Your name');
+    const button = getByText('Send');
+
+    fireEvent.changeText(input, 'allex');
+    fireEvent.press(button);
+
+    expect(mockNavigator).toHaveBeenCalled();
   });
 });
